2dbf072d314b0913b08c9a52b236d47617a12684
Andrey Smirnov <ndreys@bulbasaur.(none)>
2012-11-18 12:28:11 -0800

    Add support for Application programming
    This commits adds capability of downloading the firmware into the
    Application Memory. Unfortunately the code does not work for System
    Memory and it is yet to be discovered if it is even possible to do
    in-system System Memory re-flashing.

--- dfu.c
+++ dfu.c
@@ -24,6 +24,7 @@
 #include <string.h>
 #include <stdbool.h>
 
+#include <libopencm3/stm32/f1/flash.h>
 #include <libopencm3/usb/usbd.h>
 
 #include "dfu.h"
@@ -63,6 +64,12 @@ struct stfub_dfu {
 	} block;
 
 	const struct stfub_memory_bank *bank;
+
+	struct {
+		int block_no;
+		int block_len;
+		u8 block[2048];
+	} pending;
 };
 
 static struct stfub_dfu dfu;
@@ -74,6 +81,7 @@ void stfub_dfu_init(const struct usb_dfu_descriptor *descr)
 	dfu.descr	= descr;
 	dfu.timeout	= 100;
 	dfu.bank	= &stfub_memory_banks[STFUB_AS_MAIN_MEMORY];
+	dfu.pending.block_len = -1;
 }
 
 void stfub_dfu_switch_altsetting(usbd_device *usbd_dev, u16 interface, u16 altsetting)
@@ -100,51 +108,56 @@ static int stfub_dfu_read_firmware_block(struct stfub_dfu *dfu, u16 block_no,
 	return read_len;
 }
 
-static int stfub_dfu_write_firmware_block(struct stfub_dfu *dfu, u16 block_no,
-					  const u8 *buf, int len)
+static int stfub_dfu_write_firmware_block(struct stfub_dfu *dfu)
 {
-	/*
-	   FIXME: I feel like this code is not handling all the edge
-	   cases. Someday it will have to be improved.
-	 */
+	stfub_printf("stfub_dfu_write_firmware_block\n");
 
-#if 0
-	int write_len, i;
-	const u8 *start_address  = (const u8 *)0x08000000;
-	const u8 *end_address    = (const u8 *)0x08040000;
+	if (dfu->bank == &stfub_memory_banks[STFUB_AS_OPTION_BYTES]) {
+		/* Option bytes are a special case, handle them separately */
+		return -1;
+	} else {
+		int write_len, i;
 
-	static const u8 *block;
+		u8 *start_address  = (u8 *)dfu->bank->start;
+		u8 *end_address    = (u8 *)dfu->bank->end;
 
-	if (block_no == 0)
-		block = start_address;
-
-	write_len = MIN(len, end_address - block);
-
-	flash_unlock();
-
-	/* Clear the least significat bit to get the highest even
-	 * number less or eqal to write_len */
-	for (i = 0; i < (write_len & (~0x1)); i += 2)
-		flash_program_half_word((u32)(block + i), *(u16 *)(buf + i));
-
-	if (i != write_len) {
-		/* It looks like len is odd and therefor the last byte
-		 * was not written do it here*/
-		/*
-		   FIXME: Should it be
-		   u16 word = buf[len - 1] << 8;
-		   ?
+		/* 
+		   It is reasonable to assume that since the transfer
+		   length is even and the size ARM code is always
+		   divisible at least by two(each instruction is 16
+		   bits in Thumb mode and 32 in non-Thumb) and DFU
+		   suffix contains even number of bytes, in the even
+		   of len parameter being odd, that something went
+		   terribly wrong. 
 		 */
-		u16 word = buf[write_len - 1];
-		flash_program_half_word((u32)(block + i), word);
+		if (dfu->pending.block_len % 2) 
+			return -1;
+
+		if (dfu->pending.block_no == 0)
+			dfu->block.writeptr = start_address;
+
+		stfub_printf("[%d] dfu->block.writeptr = %x\n",
+			     dfu->pending.block_no, dfu->block.writeptr);
+
+		write_len = MIN(dfu->pending.block_len,
+				end_address - dfu->block.writeptr);
+
+		flash_unlock();
+
+		flash_erase_page((u32)dfu->block.writeptr);
+
+		for (i = 0; i < write_len; i += 2)
+			flash_program_half_word((u32)(dfu->block.writeptr + i),
+						*(u16 *)(dfu->pending.block + i));
+		
+		flash_lock();
+
+		dfu->block.writeptr += write_len;
+
+
+		dfu->pending.block_len = -1;
+		return 0;
 	}
-
-	flash_lock();
-
-
-	block += write_len;
-#endif
-	return 0;
 }
 
 static bool stfub_dfu_attribute_is_set(struct stfub_dfu *dfu, u8 attribute)
@@ -215,15 +228,24 @@ static void stfub_dfu_timestamp_poll_request(struct stfub_dfu *dfu)
 }
 
 static int stfub_dfu_queue_firmware_block(struct stfub_dfu *dfu,
-					   u16 block_no, const u8 *buf,
-					   int len)
+					  u16 block_no, const u8 *buf,
+					  int len)
 {
+
+	if ((unsigned int) len > sizeof(dfu->pending.block))
+		return -1;
+
+	dfu->pending.block_no = block_no;
+	memcpy(dfu->pending.block, buf, len);
+	
+	dfu->pending.block_len = len;
+
 	return 0;
 }
 
 static bool stfub_dfu_write_pending(struct stfub_dfu *dfu)
 {
-	return false;
+	return dfu->pending.block_len != -1;
 }
 
 
@@ -234,26 +256,30 @@ static bool dfu_all_data_is_received(struct stfub_dfu *dfu)
 
 void stfub_dfu_tick(void)
 {
-#if 0
 	switch(stfub_dfu_get_state(&dfu)) {
 	case STATE_DFU_DNBUSY:
-		if (stfub_dfu_write_pending(&dfu))
-			stfub_dfu_write_firmware_block(&dfu);
+		if (stfub_dfu_write_pending(&dfu)) 
+			if (stfub_dfu_write_firmware_block(&dfu) < 0)  {
+				stfub_dfu_set_state(&dfu, STATE_DFU_ERROR);
+				break;
+			}
+			
 		stfub_dfu_set_state(&dfu, STATE_DFU_DNLOAD_SYNC);
 		break;
+#if 0
 	case STATE_DFU_MANIFEST:
-		if (stfub_dfu_timeout_elapsed(&dfu)) {
+		if (stfub_dfu_timeout_elapsed(&dfu)){ 
 			if (stfub_dfu_attribute_is_set(&dfu, USB_DFU_MANIFEST_TOLERANT))
 				stfub_dfu_set_state(&dfu, STATE_DFU_MANIFEST_SYNC);
 			else
 				stfub_dfu_set_state(&dfu, STATE_DFU_MANIFEST_WAIT_RESET);
 		}
 		break;
+#endif
 	default:
 		break;
 
 	}
-#endif
 }
 
 int stfub_dfu_handle_control_request(usbd_device *udbddev, struct usb_setup_data *req, u8 **buf,
@@ -315,10 +341,11 @@ int stfub_dfu_handle_control_request(usbd_device *udbddev, struct usb_setup_data
 	case STATE_DFU_DNLOAD_SYNC:
 		switch (req->bRequest) {
 		case DFU_GETSTATUS:
-			/* Since we are working in a polling mode the
-			 * transfer will always be complete by the
-			 * time this request is served */
-			stfub_dfu_set_state(&dfu, STATE_DFU_DNLOAD_IDLE);
+			if (stfub_dfu_write_pending(&dfu))
+				stfub_dfu_set_state(&dfu, STATE_DFU_DNBUSY);
+			else
+				stfub_dfu_set_state(&dfu, STATE_DFU_DNLOAD_IDLE);
+
 			return stfub_dfu_handle_get_status_request(&dfu, *buf, len);
 		case DFU_GETSTATE:
 			return stfub_dfu_handle_get_state_request(&dfu, *buf, len);
@@ -378,8 +405,10 @@ int stfub_dfu_handle_control_request(usbd_device *udbddev, struct usb_setup_data
 				 * manifestation will always be complete by the
 				 * time this request is served */
 				stfub_dfu_set_state(&dfu, STATE_DFU_IDLE);
-				return stfub_dfu_handle_get_status_request(&dfu, *buf, len);
+			} else {
+				stfub_dfu_set_state(&dfu, STATE_DFU_MANIFEST_WAIT_RESET);
 			}
+			return stfub_dfu_handle_get_status_request(&dfu, *buf, len);
 		default:	/* FALLTHROUGH */
 			break;
 		}
